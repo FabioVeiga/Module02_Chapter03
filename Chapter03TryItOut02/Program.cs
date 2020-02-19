a51f1ebaf09df265dd18051441d1d8025ea7d294
FabioVeiga <droidbinho@gmail.com>
2020-02-19 18:55:55 -0500

    Commit
    Chapter03
    TryOut 01 and 02

---
+++ Chapter03TryItOut02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter03TryItOut02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber, secondNumber;
+            string userName;
+            Console.Write("Enter your name: ");
+            userName = Console.ReadLine();
+            Console.WriteLine("Welcome {0}!", userName);
+            Console.Write("Now give me a number: ");
+            firstNumber = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Now give me another number: ");
+            secondNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("The sum of {0} and {1} is {2}.", firstNumber, secondNumber, firstNumber + secondNumber);
+            Console.WriteLine("The result of substracting {0} and {1} is {2}.", firstNumber, secondNumber, firstNumber - secondNumber);
+            Console.WriteLine("The the product of {0} and {1} is {2}.", firstNumber, secondNumber, firstNumber * secondNumber);
+            Console.WriteLine("The result of dividing {0} and {1} is {2}.", firstNumber, secondNumber, firstNumber / secondNumber);
+            Console.WriteLine("The remainder after {0} by {1} is {2}.", firstNumber, secondNumber, firstNumber % secondNumber);
+            Console.ReadKey();
+        }
+    }
+}
